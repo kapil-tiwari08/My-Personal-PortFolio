dec855ca9416177ead9c6b2c037be05df42c7390
kapil-tiwari08 <kapilkappy2002@gmail.com>
2025-06-07 02:04:24 +0530

    left - right animation

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -21,25 +21,23 @@ const Education = () => {
         </h1>
 
         <div
-          className="EDUCATION flex gap-10 justify-between flex-row-reverse max-sm:flex-col-reverse max-sm:items-center"
+          className="EDUCATION flex gap-10 justify-between flex-row max-sm:flex-col max-sm:items-center"
           data-aos="fade-right"
         >
-          {/* Lottie Animation */}
-          <Lottie
-            animationData={educationAnimation}
-            loop={true}
-            className="max-w-[500px] w-full max-sm:max-w-[300px] max-sm:mb-5 shadow-xl rounded-xl border border-[#00040f]"
-          />
+          {/* Lottie Animation (Left Side) */}
+          <div className="max-w-[500px] w-full max-sm:max-w-[300px] max-sm:mb-5 shadow-xl rounded-xl border border-[#00040f]">
+            <Lottie animationData={educationAnimation} loop={true} />
+          </div>
 
-          {/* College Details */}
+          {/* College Details (Right Side) */}
           <div className="max-w-[520px] mt-[52px] p-7 max-sm:p-4 max-sm:mt-2">
-            <div className="flex gap-5 max-sm:flex-col max-sm:items-center text-center max-sm:text-left">
+            <div className="flex gap-5 max-sm:flex-col max-sm:items-center text-left">
               <img
                 src={JEC}
                 alt="JEC"
                 className="w-[90px] h-[90px] max-sm:w-[70px] max-sm:h-[70px]"
               />
-              <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 dark:from-[#ff6600] dark:to-slate-100 font-semibold text-2xl max-sm:text-[18px] tracking-wide max-w-[300px]">
+              <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 dark:from-[#ff6600] dark:to-slate-100 font-semibold text-2xl max-sm:text-[18px] tracking-wide">
                 Jabalpur Engineering College, Jabalpur, Madhya Pradesh
               </h1>
             </div>
